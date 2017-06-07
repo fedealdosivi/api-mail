3876d582e7fc7c7f7480ac48b2cf9727a109e025
fefe <fedepalo13@gmail.com>
2017-06-07 19:06:33 -0300

    Primer commit

---
+++ apimail/src/main/java/apimail/App.java
@@ -0,0 +1,13 @@
+package apimail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
