c5bee2edc450c6391b2e3e83b726a1200039183c
fefe <fedepalo13@gmail.com>
2017-06-09 14:02:50 -0300

    new classes

---
+++ apimail/src/main/java/apimail/Request/MensajeRequest.java
@@ -0,0 +1,7 @@
+package apimail.Request;
+
+/**
+ * Created by fefe on 8/6/2017.
+ */
+public class MensajeRequest {
+}
---
+++ apimail/src/main/java/apimail/Response/UsuarioResponse.java
@@ -0,0 +1,7 @@
+package apimail.Response;
+
+/**
+ * Created by fefe on 8/6/2017.
+ */
+public class UsuarioResponse {
+}
