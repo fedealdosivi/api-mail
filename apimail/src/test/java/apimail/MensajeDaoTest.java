3444bb4206abb6218c62194471a637b3b83d1a37
fefe <fedepalo13@gmail.com>
2017-06-13 20:58:01 -0300

    primera instancia de los test

---
+++ apimail/src/test/java/apimail/MensajeDaoTest.java
@@ -0,0 +1,7 @@
+package apimail;
+
+/**
+ * Created by fefe on 13/6/2017.
+ */
+public class MensajeDaoTest {
+}
---
+++ apimail/src/test/java/apimail/UsuarioDaoTest.java
@@ -0,0 +1,8 @@
+package apimail;
+
+/**
+ * Created by fefe on 13/6/2017.
+ */
+public class UsuarioDaoTest
+{
+}
