ce36c5cb2405732c2d7ed6488c296e47dfe7e0d2
fefe <fedepalo13@gmail.com>
2017-06-14 13:41:38 -0300

    dependencias test integral

---
+++ apimail/src/test/java/apimail/MensajeControlIT.java
@@ -0,0 +1,12 @@
+package apimail;
+
+/**
+ * Created by fefe on 14/6/2017.
+ */
+import junit.framework.Test;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+public class MensajeControlIT {
+}
---
+++ apimail/src/test/java/apimail/UsuarioControlIT.java
@@ -0,0 +1,7 @@
+package apimail;
+
+/**
+ * Created by fefe on 14/6/2017.
+ */
+public class UsuarioControlIT {
+}
